e67e101063d675403d2319f0de242c679d4406ea
KSN <ksn_i777@mail.ru>
2022-11-14 16:55:40 +0300

    tuesday-2-lesson-14-refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Title} from "./components/Title/Title";
 import {Accordion} from "./components/Accordion/Accordion";
 import {UncontrolledAccordion} from './components/UncontrolledAccordion/UncontrolledAccordion';
 import {Rating} from "./components/Rating/Rating";
@@ -8,8 +7,6 @@ import {UncontrolledRating} from './components/UncontrolledRating/UncontrolledRa
 import {OnOff} from './components/OnOff/OnOff';
 import {UncontrolledOnOff} from './components/UncontrolledOnOff/UncontrolledOnOff';
 import {Select} from './components/Select/Select';
-import {Simulate} from 'react-dom/test-utils';
-import change = Simulate.change;
 
 export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5;
 export type StatusOnOff = 'on' | 'off' | 'reset';
@@ -21,10 +18,21 @@ export type Items = Array<Item>
 
 function App() {
 
-    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
-    const [deploy, setDeploy] = useState<boolean>(false)
-    const [status, setStatus] = useState<StatusOnOff>('reset')
-    const [itemValue, setItemValue] = useState<number>(1)
+    const [deploy, setDeploy] = useState<boolean>(false) //Accordion
+
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0) //Rating
+
+    const [status, setStatus] = useState<StatusOnOff>('reset') //OnOff
+
+    const [itemValue, setItemValue] = useState<number>(1) //Select
+
+    function clickBodyItem(title: string) {
+        alert(`${title} here`)
+    } //Accordion
+
+    function changeItem(itemID: number) {
+        setItemValue(itemID)
+    } //Select
 
     const items:Items = [
         {id: 1, title: 'HTML'},
@@ -33,18 +41,8 @@ function App() {
         {id: 4, title: 'React'},
     ]
 
-    function clickBodyItem(title: string) {
-        alert(`${title} here`)
-    }
-
-    function changeItem(itemID: number) {
-        setItemValue(itemID)
-    }
-
     return (
         <div className="App">
-            <Title title={"This APP component"}/>
-
             <Accordion title={"Disciplines"} items={items} deploy={deploy} changeDeploy={setDeploy} clickBodyItem={clickBodyItem}/>
             <UncontrolledAccordion title={"Menu"}/>
 
--- src/components/Title/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type TitlePropsType = {
-    title: string,
-}
-
-export function Title(props: TitlePropsType) {
-    return <h2>{props.title}</h2>
-}
