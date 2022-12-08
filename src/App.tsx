2c3502d9098c9bce216ffb96b41e3d62342e1624
KSN <ksn_i777@mail.ru>
2022-12-08 15:33:51 +0300

    wednesday-2-lesson-22

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
 //import {MemoAccordion} from './components/Accordion/Accordion';
 //import {MemoUncontrolledAccordion} from './components/UncontrolledAccordion/UncontrolledAccordion';
@@ -27,11 +27,11 @@ function App() {
     //const [ratingValue, setRatingValue] = useState<RatingValueType>(0) //MemoRating
     //const [status, setStatus] = useState<StatusOnOff>('reset') //MemoOnOff
 
-    const [idCity, setIdCity] = useState<number>(0) //MemoSelect
-    const [idCity2, setIdCity2] = useState<number>(0) //MemoSelect
-    const [idCity3, setIdCity3] = useState<number>(0) //MemoSelect
+    //const [idCity, setIdCity] = useState<number>(0) //MemoSelect
+    //const [idCity2, setIdCity2] = useState<number>(0) //MemoSelect
+    //const [idCity3, setIdCity3] = useState<number>(0) //MemoSelect
 
-    const cities:Cities = [
+    /*const cities:Cities = [
         {id: 1, city: 'Minsk', country: "Belarus", population: 5},
         {id: 2, city: 'Mogilev', country: "Belarus", population: 3},
         {id: 3, city: 'Grodno', country: "Belarus", population: 2},
@@ -51,8 +51,20 @@ function App() {
     }, [])
     const cities5 = useMemo(() => {
         return cities.filter(s => s.population < 5)
+    }, [])*/
+
+    const [date, setDate] = useState(new Date())
+
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
     }, [])
-    
+
     return (
         <div className="App">
             {/* <MemoAccordion title={"Disciplines"} deploy={deploy} changeDeploy={setDeploy}/>
@@ -64,9 +76,11 @@ function App() {
             <MemoOnOff status={status} changeStatus={setStatus}/>
             <MemoUncontrolledOnOff/> */}
 
-            <MemoSelect idCity={idCity} setIdCity={setIdCity} cities={citiesB} selectNumber={1}/>
+            {/*<MemoSelect idCity={idCity} setIdCity={setIdCity} cities={citiesB} selectNumber={1}/>
             <MemoSelect idCity={idCity2} setIdCity={setIdCity2} cities={citiesM} selectNumber={2}/>
-            <MemoSelect idCity={idCity3} setIdCity={setIdCity3} cities={cities5} selectNumber={3}/>
+            <MemoSelect idCity={idCity3} setIdCity={setIdCity3} cities={cities5} selectNumber={3}/>*/}
+
+            {hours+':'+minutes+':'+seconds}
         </div>
     );
 }
